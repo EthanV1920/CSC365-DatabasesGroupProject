6e3e61b80b97a10b02535dce5fe327b269748633
Ethan Vosburg <ethan@vosburg.us>
2024-06-03 03:51:39 -0700

    Broken Match SQL
    Working SQL in an editor but it does not work in the app

--- src/api/match.py
+++ src/api/match.py
@@ -1,4 +1,3 @@
-import openai as OpenAI
 from pathlib import Path
 from fastapi import APIRouter, Depends, Request
 from src.api import auth
@@ -8,39 +7,120 @@ import sqlalchemy
 
 from src import database as db
 
-# client = OpenAI()
-GPT_MODEL = "gpt-3.5-turbo"
-
 router = APIRouter()
 
 
-@router.get("/userNew/", tags=["userNew"])
-def dbstats(username: str = ""):
+@router.get("/match/create/", tags=["match"])
+def matchCreate(user_id: int):
+
+    match_sql = sqlalchemy.text("""
+with
+  user_level as (
+    select
+      level
+    from
+      users
+    where
+      user_id = 2
+  ),
+  opponent as (
+    select
+      nullif(
+        user_id,
+        (
+          select
+            player2
+          from
+            matches
+          where
+            player2 = 2
+            and player2_char is null
+        )
+      ) as user_id
+    from
+      users,
+      user_level
+    where
+      users.level between user_level.level - 1 and user_level.level  + 1
+      and user_id != 2
+      and (
+        select
+          online
+        from
+          users
+        where
+          user_id = 2
+      )
+      and online = true
+    order by
+      random()
+    limit
+      1
+  )
+insert into
+  matches (
+    player1,
+    player1_char,
+    player2,
+    player2_char,
+    status
+  )
+values
+  (
+    2,
+    1,
+    (
+      select
+        *
+      from
+        opponent
+    ),
+    null,
+    -1
+  )
+returning
+  player2;
+
+do $$
+    begin
+        if exists(select player2 from matches where player2 = 2) then
+            raise exception 'Match Already Exists';
+            rollback;
+
+        end if;
+    end
+$$;
+                                """)
 
     with db.engine.begin() as connection:
         # temporarily random, will add more logic in a later implementation
-        opponent = connection.execute("""
-            SELECT user_id FROM users WHERE online = TRUE ORDER BY RANDOM() LIMIT 1
-        """).fetchone()
-        user_id = connection.execute("SELECT user_id FROM users WHERE username = :username", {
-                                     "username": username}).fetchone()
-        match_id = connection.execute(
-            """
-                INSERT INTO matches (player1, player2, result)
-                VALUES (:user_id, :opponent, 0)
-                RETURNING match_id
-            """, {"user_id": user_id, "opponent": opponent}
-        )
+        # selection parameters:
+        #   should be similar level
+        #   must be online
+        try:
+            result = connection.execute(match_sql, {"user_id": user_id}).fetchone()
+        except Exception as e:
+            print(f"Exception: {e}")
+
+        # connection.execute(match_sql)
+        print(f"Result: {result}")
+
+        if result is None:
+            return {"error": "Match failed to create, check status and existence of players"}
+
+
+        result_string = f"Match started with {user_id} who added {result.player2}"
+        print(result_string)
 
     return [
         {
-            "match_id": match_id,
-            "opponent_id": opponent
+            "match_id": "match",
+            "opponent_id": "opponent"
         }
     ]
 
 
-@router.put("/match/updateWinner/", tags=["matchUpdate"])
+@router.put("/match/updateWinner/", tags=["match"])
 def match_end(winner: int, match_id: int):
     with db.engine.begin() as connection:
         match = connection.execute("SELECT match_id FROM matches WHERE match_id = :match_id", {
@@ -53,45 +133,3 @@ def match_end(winner: int, match_id: int):
                                """, {"winner": winner, "match_id": match_id})
 
     return {"success": "Successfully updated match"}
-
-
-@router.get("/insult/", tags=["insult"])
-def getInsult(player: str = "", game_end_state: str = "", opponent: str = ""):
-    messages = [
-        {
-            "role": "system",
-            "content": "You are a helpful ai that will assist a user who is playing Mortal Kombat 11 in insulting their opponent in a tongue-in-cheek way. You will be given the oponents name and weather or not the user won as well as the player that the user was using. You must create an insult that is witty and funny that can be put though a text to speech program and be said out loud. The response should only contain the insult."
-        },
-        {
-            "role": "user",
-            "content": f"My player was {player} and I {game_end_state} the match, my opponent was {opponent}. Can you help me come up with a funny insult that I could tell the person who I played with?"
-        }
-
-    ]
-
-    insult = chat_completion_request(messages)
-
-    speech_file_path = Path(__file__).parent / "speech.mp3"
-    response = client.audio.speech.create(
-            model="tts-1",
-            voice="alloy",
-            input=insult
-            )
-
-    response.stream_to_file(speech_file_path)
-
-    return insult
-
-
-def chat_completion_request(messages, tools=None, model=GPT_MODEL):
-    try:
-        response = client.chat.completions.create(
-            model=model,
-            messages=messages,
-            tools=tools,
-        )
-        return response
-    except Exception as e:
-        print("Unable to generate ChatCompletion response")
-        print(f"Exception: {e}")
-        return e
