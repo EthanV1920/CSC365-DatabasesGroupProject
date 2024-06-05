41a726620a74a0d73d2faa98c056f3fc84471267
Ethan Vosburg <ethan@vosburg.us>
2024-06-05 09:47:23 -0700

    Match Logic is working

--- src/api/match.py
+++ src/api/match.py
@@ -11,52 +11,37 @@ router = APIRouter()
 
 
 @router.get("/match/create/", tags=["match"])
-def matchCreate(user_id: int):
+def matchCreate(user_id: int, user_char: int):
+
+    check_sql = sqlalchemy.text("""
+                                select
+                                id
+                                from
+                                matches
+                                where
+                                (
+                                    player1 = :user_id
+                                    or player2 = :user_id
+                                    )
+                                and status = -1
+                                """)
 
     match_sql = sqlalchemy.text("""
-with
-  user_level as (
-    select
-      level
-    from
-      users
-    where
-      user_id = 2
-  ),
-  opponent as (
-    select
-      nullif(
-        user_id,
-        (
-          select
-            player2
-          from
-            matches
-          where
-            player2 = 2
-            and player2_char is null
-        )
-      ) as user_id
-    from
-      users,
-      user_level
-    where
-      users.level between user_level.level - 1 and user_level.level  + 1
-      and user_id != 2
-      and (
-        select
-          online
-        from
-          users
-        where
-          user_id = 2
-      )
-      and online = true
-    order by
-      random()
-    limit
-      1
-  )
+                                with user_level as (select level
+                    from users
+                    where user_id = :user_id),
+     opponent
+         as (select nullif(user_id, (select player2 from matches where player2 = :user_id and player2_char is null)) as user_id
+             from users,
+                  user_level
+             where users.level between user_level.level - 5 and user_level.level + 5
+               and user_id != :user_id
+               and (select online
+                    from users
+                    where user_id = :user_id) = true
+               and online = true
+             order by random()
+             limit 1)
 insert into
   matches (
     player1,
@@ -67,8 +52,8 @@ insert into
   )
 values
   (
-    2,
-    1,
+    :user_id,
+    :user_char,
     (
       select
         *
@@ -79,17 +64,8 @@ values
     -1
   )
 returning
+  id,
   player2;
-
-do $$
-    begin
-        if exists(select player2 from matches where player2 = 2) then
-            raise exception 'Match Already Exists';
-            rollback;
-
-        end if;
-    end
-$$;
                                 """)
 
     with db.engine.begin() as connection:
@@ -97,10 +73,13 @@ $$;
         # selection parameters:
         #   should be similar level
         #   must be online
-        try:
-            result = connection.execute(match_sql, {"user_id": user_id}).fetchone()
-        except Exception as e:
-            print(f"Exception: {e}")
+        is_open_match = connection.execute(check_sql, {"user_id": user_id}).fetchone()
+        print(is_open_match)
+
+        if is_open_match is None:
+            result = connection.execute(match_sql, {"user_id": user_id, "user_char": user_char}).fetchone()
+        else:
+            return {"error": "There is already an open match with this player"}
 
         # connection.execute(match_sql)
         print(f"Result: {result}")
@@ -114,22 +93,43 @@ $$;
 
     return [
         {
-            "match_id": "match",
-            "opponent_id": "opponent"
+            "match_id": result.id,
+            "opponent_id": result.player2
         }
     ]
 
 
-@router.put("/match/updateWinner/", tags=["match"])
-def match_end(winner: int, match_id: int):
+@router.put("/match/joinMatch/", tags=["match"])
+def match_join(user_id: int, user_char: int):
+
+    join_sql = sqlalchemy.text("""
+                               update matches
+                               set
+                               player2_char = :user_char,
+                               status = 0
+                               where
+                               player2 = :user_id
+                               and status = -1
+                               """)
     with db.engine.begin() as connection:
-        match = connection.execute("SELECT match_id FROM matches WHERE match_id = :match_id", {
-                                   "match_id": match_id}).fetchone()
+        connection.execute(join_sql, {"user_char": user_char, "user_id": user_id})
+
+    return {"success": "Successfully joined match"}
+
+
+@router.put("/match/updateWinner/", tags=["match"])
+def match_end(match_status: int, match_id: int):
+
+    check_sql = sqlalchemy.text("SELECT id FROM matches WHERE id = :match_id")
+
+    update_sql = sqlalchemy.text("UPDATE matches SET status = :match_status WHERE id = :match_id")
+
+    with db.engine.begin() as connection:
+        match = connection.execute(check_sql, {
+                                   "match_id": match_id}).scalar()
         if match is None:
             return {"error": "Match not found"}
         else:
-            connection.execute("""
-                               UPDATE matches SET result = :winner WHERE match_id = :match_id
-                               """, {"winner": winner, "match_id": match_id})
+            connection.execute(update_sql, {"match_status": match_status, "match_id": match_id})
 
-    return {"success": "Successfully updated match"}
+    return {"success": f"Successfully updated match {match}"}
