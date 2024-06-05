01985337b9c259104062ec05ec016a99d18a552b
IsaacLake03 <156034639+IsaacLake03@users.noreply.github.com>
2024-06-04 23:16:16 -0700

    K

--- fake_data_gen/fake_char.py
+++ fake_data_gen/fake_char.py
@@ -2,6 +2,8 @@ from faker import Faker
 import sqlalchemy
 import database as db
 import random
+from sqlalchemy import text
+
 
 fake = Faker()
 
@@ -19,34 +21,73 @@ fake = Faker()
 #         connection.execute(sqltxt)
 #     print(result)
 
+#Char inventory generation 1001131
+fake = Faker(['en_US'])
+params = []
+
+# Fetch valid user IDs from the database
+with db.engine.begin() as connection:
+    result = connection.execute(text("""
+        SELECT user_id FROM users
+    """))
+    valid_user_ids = [row[0] for row in result]
+
+# Loop over valid user IDs
+for user_id in valid_user_ids:
+    chars = []
+    for _ in range(10):
+        char_id = fake.random_int(min=38, max=74)
+        if char_id not in chars:
+            chars.append(char_id)
+            params.append({"user_id": user_id, "char_id": char_id})
+
+    # Execute the SQL statements in batches of 10000
+    if len(params) >= 10000:
+        with db.engine.begin() as connection:
+            connection.execute(text("""
+                INSERT INTO characters_ledger (user_id, character_id) 
+                VALUES (:user_id, :char_id)
+            """), params)
+        print("sent 10000")
+        params = []
+
+# Execute any remaining SQL statements
+if params:
+    with db.engine.begin() as connection:
+        connection.execute(text("""
+            INSERT INTO characters_ledger (user_id, character_id) 
+            VALUES (:user_id, :char_id)
+        """), params)
+
+
 
 #match generation
-for _ in range(10):
-    result = ""
-    wincount = 0
-    losscount = 0
-    tiecount = 0
-    fake = Faker(['en_US'])
-    for _ in range(100000):
-        player = fake.random_int(min=1, max=1001131)
-        opponent = fake.random_int(min=1, max=1001131)
-        while player == opponent:
-            opponent = fake.random_int(min=1, max=1001131)
-        weights = [45, 45, 10]  # weights for 1, 2, and 3
-        status = random.choices([1, 2, 3], weights=weights, k=1)[0]
-        if status == 1:
-            wincount += 1
-        elif status == 2:
-            losscount += 1
-        else:
-            tiecount += 1
-        player_char1 = fake.random_int(min=38, max=74)
-        player_char2 = fake.random_int(min=38, max=74)
-        result += "INSERT INTO matches (player1, player2, status, player_char1, player_char2) VALUES (" + str(player) + " , " + str(opponent) + " , '" + str(status) + "' , " + str(player_char1) + " , " + str(player_char2) +");"
-    # with db.engine.begin() as connection:
-    #     sqltxt = sqlalchemy.text(result)
-    #     connection.execute(sqltxt)
-    print(result)
-    print(str(wincount) + " " + str(losscount) + " " + str(tiecount))
+# for _ in range(10):
+#     result = ""
+#     wincount = 0
+#     losscount = 0
+#     tiecount = 0
+#     fake = Faker(['en_US'])
+#     for _ in range(100000):
+#         player = fake.random_int(min=1, max=1001131)
+#         opponent = fake.random_int(min=1, max=1001131)
+#         while player == opponent:
+#             opponent = fake.random_int(min=1, max=1001131)
+#         weights = [45, 45, 10]  # weights for 1, 2, and 3
+#         status = random.choices([1, 2, 3], weights=weights, k=1)[0]
+#         if status == 1:
+#             wincount += 1
+#         elif status == 2:
+#             losscount += 1
+#         else:
+#             tiecount += 1
+#         player_char1 = fake.random_int(min=38, max=74)
+#         player_char2 = fake.random_int(min=38, max=74)
+#         result += "INSERT INTO matches (player1, player2, status, player_char1, player_char2) VALUES (" + str(player) + " , " + str(opponent) + " , '" + str(status) + "' , " + str(player_char1) + " , " + str(player_char2) +");"
+#     # with db.engine.begin() as connection:
+#     #     sqltxt = sqlalchemy.text(result)
+#     #     connection.execute(sqltxt)
+#     print(result)
+#     print(str(wincount) + " " + str(losscount) + " " + str(tiecount))
 
 
